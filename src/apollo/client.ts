0d80a0ec63ccc631b881dcde55c8535438dc7b61
benesjan <janbenes1234@gmail.com>
2021-06-14 14:58:51 +0200

    feat: fetching x days old block

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -12,3 +12,23 @@ export const client = new ApolloClient({
         },
     },
 });
+
+export const healthClient = new ApolloClient({
+    uri: 'https://api.thegraph.com/index-node/graphql',
+    cache: new InMemoryCache(),
+});
+
+export const blockClient = new ApolloClient({
+    uri: 'https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks',
+    cache: new InMemoryCache(),
+    queryDeduplication: true,
+    defaultOptions: {
+        watchQuery: {
+            fetchPolicy: 'network-only',
+        },
+        query: {
+            fetchPolicy: 'network-only',
+            errorPolicy: 'all',
+        },
+    },
+});
--- src/fees/utils.ts
+++ src/fees/utils.ts
@@ -1,7 +1,8 @@
 import { gql } from '@apollo/client/core';
-import { client } from '../apollo/client';
+import { blockClient, client } from '../apollo/client';
+import dayjs from 'dayjs';
 
-const BLOCK_QUERY = gql`
+const LATEST_INDEXED_BLOCK_QUERY = gql`
     query block {
         _meta {
             block {
@@ -13,8 +14,32 @@ const BLOCK_QUERY = gql`
 
 export async function getLatestBlock(): Promise<number> {
     const result = await client.query({
-        query: BLOCK_QUERY,
+        query: LATEST_INDEXED_BLOCK_QUERY,
     });
 
     return Number(result.data._meta.block.number);
+}
+
+const BLOCK_QUERY = gql`
+    query block($timestamp: Int) {
+        blocks(
+            first: 1
+            where: { timestamp_gte: $timestamp }
+            orderBy: number
+            orderDirection: asc
+        ) {
+            number
+        }
+    }
+`;
+
+export async function getBlockNumDaysAgo(numDays: number): Promise<number> {
+    const result = await blockClient.query({
+        query: BLOCK_QUERY,
+        variables: {
+            timestamp: dayjs().subtract(numDays, 'day').unix(),
+        },
+    });
+
+    return Number(result.data.blocks[0].number);
 }
