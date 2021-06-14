6f2fa4c1a54d8679396059804c088535b41b29e7
benesjan <janbenes1234@gmail.com>
2021-06-14 18:46:54 +0200

    feat: initial imprecise implementation of fee estimation

--- src/fees/fee-estimation.ts
+++ src/fees/fee-estimation.ts
@@ -2,9 +2,13 @@ import { gql } from '@apollo/client/core';
 import { Position } from '@uniswap/v3-sdk';
 import { client } from '../apollo/client';
 import { getPool } from '../sdk-utils';
+import { getFeeGrowthInside, getTotalPositionFees, parseTick } from './total-user-fees';
+import { BigNumber } from 'ethers';
+import { getBlockNumDaysAgo } from './utils';
+import { formatUnits } from 'ethers/lib/utils';
 
 const FEE_ESTIMATE_QUERY = gql`
-    query feeEstimationData($pool: String, $tickLower: Int, $tickUpper: Int) {
+    query feeEstimationData($pool: String, $tickLower: Int, $tickUpper: Int, $block: Int) {
         bundle(id: "1") {
             ethPriceUSD
         }
@@ -23,38 +27,97 @@ const FEE_ESTIMATE_QUERY = gql`
             liquidity
             sqrtPrice
             tick
+            feeGrowthGlobal0X128
+            feeGrowthGlobal1X128
         }
-        tickLower: ticks(first: 1, where: { poolAddress: $pool, tickIdx_gte: $tickLower }) {
+        tickLower: ticks(
+            first: 1
+            where: { poolAddress: $pool, tickIdx_gte: $tickLower }
+            orderBy: tickIdx
+            orderDirection: asc
+        ) {
+            idx: tickIdx
             feeGrowthOutside0X128
             feeGrowthOutside1X128
         }
-        tickUpper: ticks(first: 1, where: { poolAddress: $pool, tickIdx_lte: $tickUpper }) {
+        tickUpper: ticks(
+            first: 1
+            where: { poolAddress: $pool, tickIdx_lte: $tickUpper }
+            orderBy: tickIdx
+            orderDirection: desc
+        ) {
+            idx: tickIdx
+            feeGrowthOutside0X128
+            feeGrowthOutside1X128
+        }
+        poolOld: pool(id: $pool, block: { number: $block }) {
+            token0 {
+                id
+                decimals
+                derivedETH
+            }
+            token1 {
+                id
+                decimals
+                derivedETH
+            }
+            feeTier
+            liquidity
+            sqrtPrice
+            tick
+            feeGrowthGlobal0X128
+            feeGrowthGlobal1X128
+        }
+        tickLowerOld: ticks(
+            first: 1
+            where: { poolAddress: $pool, tickIdx_gte: $tickLower }
+            orderBy: tickIdx
+            orderDirection: asc
+            block: { number: $block }
+        ) {
+            idx: tickIdx
+            feeGrowthOutside0X128
+            feeGrowthOutside1X128
+        }
+        tickUpperOld: ticks(
+            first: 1
+            where: { poolAddress: $pool, tickIdx_lte: $tickUpper }
+            orderBy: tickIdx
+            orderDirection: desc
+            block: { number: $block }
+        ) {
+            idx: tickIdx
             feeGrowthOutside0X128
             feeGrowthOutside1X128
         }
     }
 `;
 
-async function estimate24hFees(
+async function estimate24hUsdFees(
     pool: string,
     liquidityUsd: number,
     tickLower: number,
     tickUpper: number,
-    numDaysBack: number,
+    numDaysAgo: number,
 ): Promise<number> {
+    // 1. Fetch block from numDaysAgo
+    // This operation is really slow - consider replacing or calling in advance
+    const blockNumDaysAgo = await getBlockNumDaysAgo(numDaysAgo);
+
+    // 2. fetch all the other data
     let result = await client.query({
         query: FEE_ESTIMATE_QUERY,
         variables: {
             pool: pool,
             tickLower: tickLower,
             tickUpper: tickUpper,
+            block: blockNumDaysAgo,
         },
     });
 
+    // 3. convert liquidityUsd to liquidity
     const poolInstance = getPool(result.data.pool);
-
     const ethPrice = Number(result.data.bundle.ethPriceUSD);
-
     const tick = Number(result.data.pool.tick);
 
     let token0Share: number;
@@ -79,7 +142,6 @@ async function estimate24hFees(
     const token1Amount =
         (liquidityUsd / token1Price) * token1Share * 10 ** Number(result.data.pool.token1.decimals);
 
-    // 1. convert liquidityUsd to liquidity
     const position = Position.fromAmounts({
         pool: poolInstance,
         tickLower: tickLower,
@@ -89,12 +151,65 @@ async function estimate24hFees(
         useFullPrecision: true,
     });
 
-    // 2. fetch ticksLower and tickUpper - if they are uninitialized fetch
-    // the closest one within range
+    const liquidity = BigNumber.from(position.liquidity.toString());
+
+    // 4. Parse and verify data
+    const poolDataCurrent = result.data.pool;
+    const tickLowerInstanceCurrent = parseTick(result.data.tickLower[0]);
+    const tickUpperInstanceCurrent = parseTick(result.data.tickUpper[0]);
+
+    if (tickLowerInstanceCurrent.idx.gte(tickUpperInstanceCurrent.idx)) {
+        console.error('Lower tick Idx >= Upper tick Idx');
+        return 0;
+    }
+
+    const poolDataOld = result.data.poolOld;
+    const tickLowerInstanceOld = parseTick(result.data.tickLowerOld[0]);
+    const tickUpperInstanceOld = parseTick(result.data.tickUpperOld[0]);
+
+    if (tickLowerInstanceOld.idx.gte(tickUpperInstanceOld.idx)) {
+        console.error('Old lower tick Idx >= Old upper tick Idx');
+        return 0;
+    }
+
     // 3. get fee growth between given ticks
-    return 0;
+    let [feeGrowthInside0X128, feeGrowthInside1X128] = getFeeGrowthInside(
+        tickLowerInstanceCurrent,
+        tickUpperInstanceCurrent,
+        BigNumber.from(poolDataCurrent.tick),
+        BigNumber.from(poolDataCurrent.feeGrowthGlobal0X128),
+        BigNumber.from(poolDataCurrent.feeGrowthGlobal1X128),
+    );
+
+    let [feeGrowthInside0LastX128, feeGrowthInside1LastX128] = getFeeGrowthInside(
+        tickLowerInstanceOld,
+        tickUpperInstanceOld,
+        BigNumber.from(poolDataOld.tick),
+        BigNumber.from(poolDataOld.feeGrowthGlobal0X128),
+        BigNumber.from(poolDataOld.feeGrowthGlobal1X128),
+    );
+
+    let fees = getTotalPositionFees(
+        feeGrowthInside0X128,
+        feeGrowthInside1X128,
+        feeGrowthInside0LastX128,
+        feeGrowthInside1LastX128,
+        liquidity,
+    );
+
+    const feesToken0 = Number(formatUnits(fees.feesToken0, result.data.pool.token0.decimals));
+    const feesToken1 = Number(formatUnits(fees.feesToken1, result.data.pool.token1.decimals));
+
+    return (feesToken0 * token0Price + feesToken1 * token1Price) / numDaysAgo;
 }
 
 (async function main() {
-    await estimate24hFees('0x151ccb92bc1ed5c6d0f9adb5cec4763ceb66ac7f', 15902, -31980, -28320, 7);
+    const feesUsd = await estimate24hUsdFees(
+        '0x151ccb92bc1ed5c6d0f9adb5cec4763ceb66ac7f',
+        15902,
+        -31980,
+        -28320,
+        7,
+    );
+    console.log(feesUsd);
 })().catch(error => console.error(error));
