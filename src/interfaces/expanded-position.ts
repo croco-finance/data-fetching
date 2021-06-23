83f9909e8a202194b8ff2039e8e5cb573e6917f4
benesjan <janbenes1234@gmail.com>
2021-06-23 12:04:08 +0200

    feat: fetching and parsing expanded position info

--- src/interfaces/expanded-position.ts
+++ src/interfaces/expanded-position.ts
@@ -1,19 +1,14 @@
 import { DailyFees } from '../fees/daily-position-fees';
+import { CurrencyAmount, Token } from '@uniswap/sdk-core';
 
-interface Transaction {
+export interface Transaction {
     id: string; // tx hash
     timestamp: number;
     txCostETH: number;
-    ethPriceUSD: number | undefined; // price of ETH at the time this transaction happened
+    ethPriceUSD: number; // price of ETH at the time this transaction happened
 }
 
-interface Snapshot {
-    id: string; // <NFT token id>#<block number>
-
-    // how many fees did the user collect
-    collectedFeesToken0: number;
-    collectedFeesToken1: number;
-
+export interface Snapshot {
     // how many tokens did the user deposit in a given position
     depositedToken0: number;
     depositedToken1: number;
@@ -22,9 +17,13 @@ interface Snapshot {
     withdrawnToken0: number;
     withdrawnToken1: number;
 
+    // how many fees did the user collect
+    collectedFeesToken0: number;
+    collectedFeesToken1: number;
+
     // How many tokens did the user have after the change that invoked this snapshot happened
-    amountAfterToken0: number;
-    amountAfterToken1: number;
+    amountToken0: CurrencyAmount<Token>;
+    amountToken1: CurrencyAmount<Token>;
 
     // Transaction related to this snapshot
     transaction: Transaction;
@@ -35,6 +34,6 @@ export interface ExpandedPositionInfo {
     collectedFeesToken0: number;
     collectedFeesToken1: number;
 
-    snapshots: Snapshot[];
     dailyFees: DailyFees;
+    snapshots: Snapshot[];
 }
--- src/position-expanded.ts
+++ src/position-expanded.ts
@@ -4,6 +4,8 @@ import { client } from './apollo/client';
 import { computeFees } from './fees/daily-position-fees';
 import dayjs from 'dayjs';
 import { getPositions } from './positions-overview';
+import { ExpandedPositionInfo, Snapshot } from './interfaces/expanded-position';
+import { Position } from '@uniswap/v3-sdk';
 
 const POSITION_AND_SNAPS = gql`
     query positionAndSnaps($positionId: String) {
@@ -30,10 +32,12 @@ const POSITION_AND_SNAPS = gql`
             blockNumber
             timestamp
             liquidity
-            collectedFeesToken0
-            collectedFeesToken1
+            depositedToken0
+            depositedToken1
             withdrawnToken0
             withdrawnToken1
+            collectedFeesToken0
+            collectedFeesToken1
             feeGrowthInside0LastX128
             feeGrowthInside1LastX128
             transaction {
@@ -80,7 +84,9 @@ function buildQuery(
     return query;
 }
 
-async function getExpandedPosition(positionInOverview: PositionInOverview): Promise<void> {
+async function getExpandedPosition(
+    positionInOverview: PositionInOverview,
+): Promise<ExpandedPositionInfo> {
     // 1. get position and snaps
     let result = await client.query({
         query: POSITION_AND_SNAPS,
@@ -90,19 +96,19 @@ async function getExpandedPosition(positionInOverview: PositionInOverview): Prom
     });
 
     const poolId = result.data.position.pool.id;
-    const position = result.data.position;
-    const positionSnapshots = result.data.positionSnapshots;
+    const rawPosition = result.data.position;
+    const rawSnaps = result.data.positionSnapshots;
 
-    // 2. create tick ids from tickIdxes and pool address
+    // 2. create tick ids from tick indexes and pool address
     const relevantTicks: string[] = [
-        poolId.concat('#').concat(position.tickLower.tickIdx),
-        poolId.concat('#').concat(position.tickUpper.tickIdx),
+        poolId.concat('#').concat(rawPosition.tickLower.tickIdx),
+        poolId.concat('#').concat(rawPosition.tickUpper.tickIdx),
     ];
 
-    // 3. get the time from which to fetch day data
+    // 3. get the time from which to fetch day data and snap blocks
     const snapBlocks: string[] = [];
     let oldestSnapTimestamp = Number.MAX_VALUE;
-    for (const snap of positionSnapshots) {
+    for (const snap of rawSnaps) {
         snapBlocks.push(snap.blockNumber);
         const snapTimestamp = Number(snap.timestamp);
         if (snapTimestamp < oldestSnapTimestamp) {
@@ -111,18 +117,53 @@ async function getExpandedPosition(positionInOverview: PositionInOverview): Prom
     }
     const minTimestamp = Math.max(dayjs().subtract(30, 'day').unix(), oldestSnapTimestamp);
 
-    // 4. fetch positions snapshots and pool and tick day data
+    // 4. fetch positions, snapshots, pool, tick day data and eth prices in snap creation times
     result = await client.query({
         query: gql(buildQuery(poolId, minTimestamp, relevantTicks, snapBlocks)),
     });
 
-    // 5. compute fees from all the data
-    const dailyFees = computeFees(result.data, position, positionSnapshots);
-    // TODO
+    // 5. compute daily fees from all the data
+    const dailyFees = computeFees(result.data, rawPosition, rawSnaps);
+
+    // 6. process snapshots
+    let snapshots: Snapshot[] = [];
+    for (const snap of rawSnaps) {
+        const snapPosition = new Position({
+            pool: positionInOverview.pool,
+            liquidity: snap.liquidity,
+            tickLower: positionInOverview.tickLower,
+            tickUpper: positionInOverview.tickUpper,
+        });
+        snapshots.push({
+            depositedToken0: Number(snap.depositedToken0),
+            depositedToken1: Number(snap.depositedToken1),
+            withdrawnToken0: Number(snap.withdrawnToken0),
+            withdrawnToken1: Number(snap.withdrawnToken1),
+            collectedFeesToken0: Number(snap.collectedFeesToken0),
+            collectedFeesToken1: Number(snap.collectedFeesToken1),
+            amountToken0: snapPosition.amount0,
+            amountToken1: snapPosition.amount1,
+            transaction: {
+                id: snap.transaction.id,
+                timestamp: Number(snap.timestamp),
+                txCostETH: Number(snap.transaction.gasUsed) * Number(snap.transaction.gasPrice),
+                ethPriceUSD: Number(result.data['b' + snap.blockNumber].ethPriceUSD),
+            },
+        });
+    }
+
+    // 7. create ExpandedPositionInfo and return
+    return {
+        collectedFeesToken0: snapshots[snapshots.length - 1].collectedFeesToken0,
+        collectedFeesToken1: snapshots[snapshots.length - 1].collectedFeesToken1,
+        dailyFees,
+        snapshots,
+    };
 }
 
 (async function main() {
     const owners = ['0x95ae3008c4ed8c2804051dd00f7a27dad5724ed1'];
     const positions = await getPositions(owners);
     const expandedPosition = await getExpandedPosition(positions[0]);
+    console.log(expandedPosition);
 })().catch(error => console.error(error));
